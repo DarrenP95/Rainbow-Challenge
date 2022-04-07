f54984b4a86a945eb0e20179df8178153f875407
Darren Marco Paulse <95358578+DarrenP95@users.noreply.github.com>
2022-04-07 15:48:52 +0200

    Add files via upload

---
+++ Interactive Rainbow/script/rainbow.js
@@ -0,0 +1,154 @@
+// Start Red
+{
+document.getElementById("makered").style.backgroundColor ="white";
+document.getElementById("makered").style.width ="500px";
+const btn = document.getElementById('makered');
+
+let index = 0;
+
+const colors = ['red', 'white', ];
+
+btn.addEventListener('click', function onClick() {
+  btn.style.backgroundColor = colors[index];
+  btn.style.color = 'white';
+  btn.style.borderColor = 'white';
+
+  index = index >= colors.length - 1 ? 0 : index + 1;
+});
+};
+
+// End Red
+
+
+// Start Orange 
+{
+document.getElementById("makeorange").style.backgroundColor ="white"
+document.getElementById("makeorange").style.width ="500px"
+const btn = document.getElementById('makeorange');
+
+let index = 0;
+
+const colors = ['orange', 'white', ];
+
+btn.addEventListener('click', function onClick() {
+  btn.style.backgroundColor = colors[index];
+  btn.style.color = 'white';
+  btn.style.borderColor = 'white';
+
+  index = index >= colors.length - 1 ? 0 : index + 1;
+});
+};
+// End Orange 
+
+
+// Start Yellow
+{
+document.getElementById("makeyellow").style.backgroundColor ="white"
+document.getElementById("makeyellow").style.width ="500px"
+const btn = document.getElementById('makeyellow');
+
+let index = 0;
+
+const colors = ['yellow', 'white', ];
+
+btn.addEventListener('click', function onClick() {
+  btn.style.backgroundColor = colors[index];
+  btn.style.color = 'white';
+  btn.style.borderColor = 'white';
+
+  index = index >= colors.length - 1 ? 0 : index + 1;
+});
+
+};
+// End  Yellow 
+
+
+
+//Start Green
+{
+document.getElementById("makegreen").style.backgroundColor ="white"
+document.getElementById("makegreen").style.width ="500px"
+const btn = document.getElementById('makegreen');
+
+let index = 0;
+
+const colors = ['green', 'white', ];
+
+btn.addEventListener('click', function onClick() {
+  btn.style.backgroundColor = colors[index];
+  btn.style.color = 'white';
+  btn.style.borderColor = 'white';
+
+  index = index >= colors.length - 1 ? 0 : index + 1;
+});
+
+};
+// End Green 
+
+
+// Start Blue 
+{
+
+document.getElementById("makeblue").style.backgroundColor ="white"
+document.getElementById("makeblue").style.width ="500px"
+const btn = document.getElementById('makeblue');
+
+let index = 0;
+
+const colors = ['blue', 'white', ];
+
+btn.addEventListener('click', function onClick() {
+  btn.style.backgroundColor = colors[index];
+  btn.style.color = 'white';
+  btn.style.borderColor = 'white';
+
+  index = index >= colors.length - 1 ? 0 : index + 1;
+});
+
+};
+
+// End Blue 
+
+// Start Purple 
+{
+document.getElementById("makepurple").style.backgroundColor ="white"
+document.getElementById("makepurple").style.width ="500px"
+const btn = document.getElementById('makepurple');
+
+let index = 0;
+
+const colors = ['purple', 'white', ];
+
+btn.addEventListener('click', function onClick() {
+  btn.style.backgroundColor = colors[index];
+  btn.style.color = 'white';
+  btn.style.borderColor = 'white';
+
+  index = index >= colors.length - 1 ? 0 : index + 1;
+});
+};
+// End Purple 
+
+
+
+// Start Lavender 
+{
+document.getElementById("makelavender").style.backgroundColor ="white"
+document.getElementById("makelavender").style.width ="500px"
+const btn = document.getElementById('makelavender');
+
+let index = 0;
+
+const colors = [' lavender', 'white', ];
+
+btn.addEventListener('click', function onClick() {
+  btn.style.backgroundColor = colors[index];
+  btn.style.color = 'white';
+  btn.style.borderColor = 'white';
+
+  index = index >= colors.length - 1 ? 0 : index + 1;
+});
+
+
+};
+// End lavender
